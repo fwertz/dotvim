4186437ba533b3dda0153e17867e92d1cb3d94fa
Francis Wertz <francis@fwertz.(none)>
2012-03-12 15:32:11 -0400

    Changed mode on installer

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
+# Link dotvim to actual home directory and change dir to it
+ln -s $(pwd) ~/.vim
+cd !$
+
 # Static link rc files to home dir
 ln -s $(pwd)/vimrc ~/.vimrc
 ln -s $(pwd)/gvimrc ~/.gvimrc
@@ -7,8 +11,3 @@ ln -s $(pwd)/gvimrc ~/.gvimrc
 # Initiatilize && update all the things!
 git submodule init
 git submodule update
-
-
-
-
-
